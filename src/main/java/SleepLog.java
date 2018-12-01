3b381e477b24431a0fac9aa2393b93b02e7b7bb6
Cherrera543 <44183651+Cherrera543@users.noreply.github.com>
2018-11-30 23:10:16 -0600

    Update SleepLog.java

--- src/main/java/SleepLog.java
+++ src/main/java/SleepLog.java
@@ -8,9 +8,10 @@
 import java.util.*;
 import java.text.*;
 public class SleepLog {
-    private int numDays;
-    private double[] numHours;
-    private int[][] hours;
+    public int numDays;
+    public List<Double> numHours;
+    public List<Integer> night = new ArrayList<>();
+    public List<Integer> morning = new ArrayList<>();
 
     /**
      * SleepLog
@@ -31,7 +32,6 @@ public class SleepLog {
                 System.out.println("Enter in an integer.");
             }
         }
-        hours = new int[numDays][2];
         System.out.println("Enter in standard military time. Ex: 1735");
         for(int k = 0; k<numDays; k++){
             boolean sleep = false;
@@ -39,10 +39,11 @@ public class SleepLog {
             while(!sleep) {
                 try {
                     System.out.print("On night " + (k+1) + " I fell asleep at: ");
-                    hours[k][0] = Integer.parseInt(s.nextLine());
-                    if(hours[k][0]<0 || hours[k][0]>2400){
+                    int nighty= Integer.parseInt(s.nextLine());
+                    if(nighty<0 || nighty>2400){
                         throw new NumberFormatException();
                     }
+                    night.add(nighty);
                     sleep = true;
                 }catch(NumberFormatException e){
                     System.out.println("Error: Enter proper military time(0000-2400).");
@@ -51,7 +52,11 @@ public class SleepLog {
             while(!wake) {
                 try {
                     System.out.print("On night " + (k+1) + " I woke up at: ");
-                    hours[k][1] = Integer.parseInt(s.nextLine());
+                    int morn = Integer.parseInt(s.nextLine());
+                    if(morn<0 || morn>2400){
+                        throw new NumberFormatException();
+                    }
+                    morning.add(morn);
                     wake = true;
                 }catch(NumberFormatException e){
                     System.out.println("Error: Enter proper military time.");
@@ -66,14 +71,13 @@ public class SleepLog {
      * This method calculates how many hours a user slept each night. It is then returned
      * to the constructor to initialize the numHours array.
      */
-    private double[] getNumHours() {
-        numHours = new double[numDays];
+    private List<Double> getNumHours() {
         for(int k =0; k<numDays; k++){
             double sum = 0;
-            int shour = (hours[k][0])/100;
-            int sminute = (hours[k][0])%100;
-            int whour = (hours[k][1])/100;
-            int wminute = (hours[k][1])%100;
+            int shour = (night.get(k))/100;
+            int sminute = (night.get(k))%100;
+            int whour = (morning.get(k))/100;
+            int wminute = (morning.get(k))%100;
             if(shour>whour){
                 sum+=(24-shour)+whour;
             }else{
@@ -83,7 +87,7 @@ public class SleepLog {
                 sum += ((60-sminute) + wminute)/60.0;
             }
 
-            numHours[k] = sum;
+            numHours.add(sum);
         }
         return numHours;
     }
@@ -95,7 +99,7 @@ public class SleepLog {
     private int sCycles(){
         int sleepC = 0;
         for (int k =0; k<numDays; k++){
-            sleepC += (numHours[k] * 60)/90;
+            sleepC += (numHours.get(k) * 60)/90;
         }
         return sleepC;
     }
@@ -107,24 +111,35 @@ public class SleepLog {
      * then averaged out and shown as a percentage.
      */
     private String cCycle(){
-        int[] difference = {0,0};
+        List<Integer> difference = new ArrayList<>();
+        int sum1 = 0;
+        int sum2 = 0;
 
         for (int k = 0; k<numDays-1; k++){
-            for(int i = 0; i<2; i++) {
-                int hour1 = (hours[k][i]) / 100;
-                int hour2 = (hours[k + 1][i]) / 100;
+                int hour1 = night.get(k) / 100;
+                int hour2 = night.get(k) / 100;
                 if(hour1>12 && hour2>12 || hour1<12 && hour2<12){
-                    difference[i] += Math.abs(hour1-hour2);
+                    sum1 += Math.abs(hour1-hour2);
                 }else if(hour1>12){
-                    difference[i] += Math.abs(24-hour1) + hour2;
-                }else{
-                    difference[i] += Math.abs(24-hour2) + hour1;
+                    sum1 += Math.abs(24-hour1) + hour2;
+                }else {
+                    sum1 += Math.abs(24 - hour2) + hour1;
                 }
+            int hour3 = morning.get(k) / 100;
+            int hour4 = morning.get(k) / 100;
+            if(hour3>12 && hour4>12 || hour3<12 && hour4<12){
+                sum2 += Math.abs(hour3-hour4);
+            }else if(hour3>12){
+                sum2 += Math.abs(24-hour3) + hour4;
+            }else {
+                sum2 += Math.abs(24 - hour3) + hour4;
             }
         }
+        difference.add(sum1);
+        difference.add(sum2);
         DecimalFormat df = new DecimalFormat("0#.0");
         double recommended = numDays*2.0;
-        double cperc =Math.abs(100- (((difference[0]/recommended) + (difference[1]/recommended))/2.0)*100);
+        double cperc =Math.abs(100- (((difference.get(0)/recommended) + (difference.get(1)/recommended))/2.0)*100);
         df.format(cperc);
         return cperc +"%";
 
@@ -146,21 +161,17 @@ public class SleepLog {
         }
 
     }
-    /**
-     *toString
-     *This method shows sleep cycles and circadian rhythm. It is formatted so that it is in similar
-     *style to the Post object.
-     */
+
     public String toString(String nickname){
         String result = (" _________________________________________________\n" +
                 "| You slept for " + sCycles() + " sleep cycles in " + numDays + " days"+ "\t\t\t\t\t  |"+
                 "\n| Your Circadian rhythm was "+ cCycle()+"\t\t\t\t  |");
-        for(int i = 0; i<numHours.length; i++) {
-            if(numHours[i]>=10) {
-                result += "\n| Slept " + numHours[i] + " night " + (i + 1)
+        for(int i = 0; i<numHours.size(); i++) {
+            if(numHours.get(i)>=10) {
+                result += "\n| Slept " + numHours.get(i) + " night " + (i + 1)
                         + String.format("%31s", "|");
             }else{
-                result += "\n| Slept " + numHours[i] + " night " + (i + 1)
+                result += "\n| Slept " + numHours.get(i) + " night " + (i + 1)
                         + String.format("%32.5s", "|");
             }
         }
